b7d47367a13383d856c2335690bdfc7dde448031
omerbenari <omerbenari100@gmail.com>
2022-03-28 17:11:09 +0300

    new files

---
+++ 8200.py
@@ -0,0 +1,14 @@
+# print(61*74)
+# print(-7891+11039)
+# print(int(7017/3))
+# print(15-13*14+18)
+###########################################################
+digit=int(input("put your digit here:"))
+total=digit
+while total<10:
+    digit = int(input("put your digit here:"))
+    total=total+digit
+print("you reached over 10")
+
+###########################################################
+
